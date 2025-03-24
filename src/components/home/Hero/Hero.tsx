bd0460fd477d50ce059ea89ba9305cabcf64ddde
Hamilton Posada Serna <hamilton.posada@epiphany-rbc.com>
2025-03-24 13:44:08 +0100

    Update Hero component with new heading and image, and adjust styles

--- src/components/home/Hero/Hero.tsx
+++ src/components/home/Hero/Hero.tsx
@@ -1,10 +1,23 @@
+import Image from "next/image";
+
 import style from "./Hero.module.scss";
 
 export const Hero = () => {
   return (
     <section className={style.Hero}>
-      <h1>EasyFWD</h1>
+      <div></div>
+      <h1>Moving is your break!</h1>
       <h2>Empowering Your Tomorrow, Today!</h2>
+      <div className={style.Hero__imageContainer}>
+        <Image
+          src="/images/Hero.png"
+          alt="hero"
+          priority={true}
+          fill
+          placeholder="blur"
+          blurDataURL="/images/hero.jpeg"
+        />
+      </div>
     </section>
   );
 };
