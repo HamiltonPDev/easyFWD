69e33375d26aa5b26a65176552987adb4bb69dec
Omer Ulan Uluu <omer.ulanuluu@hotmail.com>
2025-03-26 00:20:36 +0100

    file and import correction

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,12 @@
 // "use client";
 import { MainProducts } from "app/components/home/MainProducts";
+import {FAQ} from "app/components/shared/FAQ";
 
 export default function Home() {
   return (
     <main>
       <MainProducts />
+      <FAQ />
     </main>
   );
 }
