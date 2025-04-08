20034e3a2a13fbdeec9721ea55699dd4dc566e96
Omer Ulan Uluu <omer.ulanuluu@hotmail.com>
2025-04-08 03:40:56 +0200

    Finished working on the user story US-12 about Services section

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,12 +1,14 @@
 // "use client";
 import { Hero } from "app/components/home/Hero";
 import { Logos } from "app/components/home/Logos";
+import { Services } from "app/components/home/Services";
 
 export default async function Home() {
   return (
     <main>
       <Hero />
       <Logos />
+      <Services />
     </main>
   );
 }
