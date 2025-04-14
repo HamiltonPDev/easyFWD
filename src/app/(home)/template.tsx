05e822cb9d8b1c33adfcde144c3ebf25d1b9d933
Omer Ulan Uluu <omer.ulanuluu@hotmail.com>
2025-04-14 19:23:24 +0200

    moving the logo section

--- src/app/(home)/template.tsx
+++ src/app/(home)/template.tsx
@@ -1,18 +1,13 @@
 "use client";
 import { SectionFaq } from "app/components/shared/SectionFaq";
-import { usePathname } from "next/navigation";
-import { Prices } from "app/components/home/PricesCards";
 
 export default function HomeTemplate({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  const pathname = usePathname();
-  const isEasyFWD = pathname === "/";
 
   return (
     <main>
       {children}
-      {isEasyFWD && <Prices />}
       <SectionFaq />
     </main>
   );
